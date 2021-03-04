b1c090ebab72686c30538202802b10020d834e27
jinchaeeun <kbg05032@naver.com>
2021-03-04 16:41:42 +0900

    버튼 클릭 시 사라짐

--- app/src/main/java/com/example/test/FrameActivity.java
+++ app/src/main/java/com/example/test/FrameActivity.java
@@ -1,14 +1,20 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FrameActivity extends AppCompatActivity {
-
+    private Button BTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_frame);
+    }
+    //버튼 클릭 시 사라지게 하기
+    public void onClick(View v) {
+        v.setVisibility(View.GONE);
     }
 }
